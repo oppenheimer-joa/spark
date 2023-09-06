1f2866cb9704a9edc6f94275c5cf8b79598b5538
blahxblah <id.blahxblah@gmail.com>
2023-09-06 12:09:24 +0900

    230906/blahxblah boxoffice 날짜별 지역취합 정렬 완료, s3 업로드 테스트 확인

--- src/BoxOffice._test.py
+++ src/BoxOffice._test.py
@@ -4,11 +4,12 @@ findspark.init()
 import pyspark
 findspark.find()
 
-from lib.modules import *
+# from lib.modules import *
+from modules import *
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, expr, when, explode, lit
-from pyspark.sql.types import IntegerType
-import json
+from pyspark.sql.functions import col, lit
+from pyspark.sql.types import IntegerType, StructType, StructField, StringType
+import json, os
 
 # Spark session 초기화
 spark = SparkSession.builder \
@@ -16,8 +17,9 @@ spark = SparkSession.builder \
     .appName("JSON to DF") \
     .getOrCreate()
     
-# 박스오피스 해당 연도의 모든 파일 목록 리스트로 반환
-def load_s3_all_files_in_dir(year):
+# 박스오피스 해당 연도의 해당날짜의 목록 리스트로 반환 - date는 YYYYMMDD 형식으로 받기
+def load_s3_all_files_in_dir(date):
+    year = date[:4]
     s3 = create_s3client()
     file_list = []
 
@@ -25,13 +27,15 @@ def load_s3_all_files_in_dir(year):
     for page in paginator.paginate(Bucket='sms-basket', Prefix=f'kobis/{year}/'):
         if "Contents" in page:
             for obj in page['Contents']:
-                file_list.append(obj['Key'])
+                file_date = obj['Key'].split('/')[-1].split('_')[0]
+                if date == file_date:
+                    file_list.append(obj['Key'])
 
     return file_list
 
 # 파일을 json 형식으로 반환
 def s3_file_to_json(file_path):
-    
+
     s3 = create_s3client()
 
     s3_object = s3.get_object(Bucket='sms-basket', Key=file_path)
@@ -42,15 +46,10 @@ def s3_file_to_json(file_path):
     
     return json_data
 
-def json_to_parquet_upload(file_path):
-    
+def json_to_df(file_path, areaCd):
+
     # 받는 file_path의 형식 정하기
     # file_path = 'kobis/2023/20230903_0105002_boxOffice.json'
-    
-    # 20230903_0105002_boxOffice
-    filename = file_path.split('/')[-1].split('.')[0]
-    # spark/kobis/2023
-    parquet_path = f'spark/{"/".join(file_path.split("/")[:2])}'
     
     json_str = s3_file_to_json(file_path)
     json_data = json.loads(json_str)
@@ -58,17 +57,66 @@ def json_to_parquet_upload(file_path):
 
     # JSON DATAFRAME 변환
     df = spark.createDataFrame(boxoffice_list)
-    sorted_df = df.withColumn("rank", col("rank").cast(IntegerType())).orderBy("rank")
-    selected_df = sorted_df.select('rnum', 'rank', 'rankInten', 'rankOldAndNew', 'movieCd', 'movieNm', 'openDt', 
+    df = df.withColumn("areaCd", lit(areaCd))
+    df = df.withColumn("rank", col("rank").cast(IntegerType()))
+    df = df.orderBy("rank")
+    df = df.select('areaCd', 'rnum', 'rank', 'rankInten', 'rankOldAndNew', 'movieCd', 'movieNm', 'openDt', 
                         'salesAmt', 'salesShare', 'salesInten', 'salesChange', 'salesAcc', 
                         'audiCnt', 'audiInten', 'audiChange', 'audiAcc', 'scrnCnt', 'showCnt')
 
-    # Parquet 파일로 저장
-    selected_df.write.parquet(f'file:///home/kjh/code/SMS/spark/data/{filename}')
+    return df
 
+def df_to_parquet(date):
+    schema = StructType([
+        StructField("areaCd", StringType(), True),
+        StructField("rnum", StringType(), True),
+        StructField("rank", IntegerType(), True),
+        StructField("rankInten", StringType(), True),
+        StructField("rankOldAndNew", StringType(), True),
+        StructField("movieCd", StringType(), True),
+        StructField("movieNm", StringType(), True),
+        StructField("openDt", StringType(), True),
+        StructField("salesAmt", StringType(), True),
+        StructField("salesShare", StringType(), True),
+        StructField("salesInten", StringType(), True),
+        StructField("salesChange", StringType(), True),
+        StructField("salesAcc", StringType(), True),
+        StructField("audiCnt", StringType(), True),
+        StructField("audiInten", StringType(), True),
+        StructField("audiChange", StringType(), True),
+        StructField("audiAcc", StringType(), True),
+        StructField("scrnCnt", StringType(), True),
+        StructField("showCnt", StringType(), True)
+    ])
+    dataframe = spark.createDataFrame([], schema)
+
+    file_list = load_s3_all_files_in_dir(date)
+    
+    # 20230903_boxOffice
+    filename = f'{date}_boxOffice'
+    
+    local_path = f'/home/kjh/code/SMS/spark/data/{filename}'
+    
+    for file_path in file_list:
+        areaCd = file_path.split("_")[1]
+        temp_df = json_to_df(file_path, areaCd)
+        dataframe = dataframe.union(temp_df)
+        
+    dataframe = dataframe.orderBy("areaCd", "rank")
+    
+    # Parquet 파일로 저장
+    dataframe.write.parquet(f'file:///home/kjh/code/SMS/spark/data/{filename}')
+
+    year = date[:4]
+    # spark/kobis/2023
+    parquet_path = f'spark/kobis/{year}'
     # S3 업로드
     s3 = create_s3client()
-    s3.upload_file(f'file:///home/kjh/code/SMS/spark/data/{filename}', 'sms-basket', f'{parquet_path}/{filename}')
+    for file in os.listdir(local_path):
+        path = os.path.join(local_path, file)
+        s3_path = os.path.join(f'{parquet_path}/{filename}', file)
+        s3.upload_file(path, 'sms-basket', s3_path)
 
-
-
+df_to_parquet('20230904')
+df = spark.read.parquet('file:///home/kjh/code/SMS/spark/data/20230904_boxOffice')
+df.show()
