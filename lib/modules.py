e838a5d28c7a96b863cc1a8cf060922c0da25fd9
J3llyBe4n <zza0423@gmail.com>
2023-09-08 17:03:57 +0900

    230908/yoda imdb 부산국제영화제 데이터 전처리 spark-job 작성 완료

--- lib/modules.py
+++ lib/modules.py
@@ -1,8 +1,8 @@
-import configparser, mysql.connector, boto3
+import configparser, mysql.connector, boto3,json
 
 def get_config(group, req_var):
 	config = configparser.ConfigParser()
-	config.read('/Users/woorek/Documents/sms/spark/config/config.ini')
+	config.read('/Users/jesse/Documents/sms/spark/config/config.ini')
 	result = config.get(group, req_var)
 
 	return result
@@ -41,3 +41,30 @@ def create_s3client():
 	s3 = boto3.client('s3', aws_access_key_id=access, aws_secret_access_key=secret)
 
 	return s3
+
+
+def make_imdb_file_dir(festa_name, year):
+	if festa_name == "academy":
+		return f'IMDb/imdb_{festa_name}_{year}.json'
+	elif festa_name == "busan":
+		return f'IMDb/imdb_{festa_name}_{year}.json'
+	elif festa_name == "cannes":
+		return f'IMDb/imdb_{festa_name}_{year}.json'
+	elif festa_name == "venice":
+		return f'IMDb/imdb_{festa_name}_{year}.json'
+	else:
+		return "wrong"
+
+def get_s3_data(file_key):
+	if file_key == "wrong":
+		return "wrong_category"
+	else:
+		s3 = create_s3client()
+
+	try:
+		obj = s3.get_object(Bucket='sms-basket', Key=file_key)
+		raw_data = obj['Body'].read()
+		json_data = json.loads(raw_data.decode('utf-8'))
+		return json.dumps(json_data)
+	except Exception as e:
+		print(str(e))
