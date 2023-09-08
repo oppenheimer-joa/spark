ba5f57d0e8c9d4960a42899b4cd2099b643f8a0d
woorek <lkjwj95@gmail.com>
2023-09-08 17:02:39 +0900

    230908/woorek imdb venice rdd & pandas 스크립트 작성(비교)

--- src/test.py
+++ src/test.py
@@ -5,10 +5,12 @@ findspark.init()
 from lib.modules import *
 
 from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StructType, StructField, StringType
 import json, os
-import pandas as pd
 from pyspark import SparkContext, SparkConf
 
+# spark = SparkSession.builder.appName("VenicetoRdd").getOrCreate()
 
 def s3_file_to_json(file_path):
 
@@ -40,20 +42,52 @@ def process_imdb_venice1(json_str): #1961~1968 & 1980~2015
         else:
             return "1"
 
-
-
-
-
-
-# Spark session 초기화
-# spark = SparkSession.builder.appName("JSON to DF").getOrCreate()
-
-# conf = SparkConf().setAppName("Complex JSON Processing")
-# sc = SparkContext(conf=conf)
-
-
 imdb_data = s3_file_to_json("IMDb/imdb_venice_1968.json")
 fn_data = process_imdb_venice1(imdb_data)
 print(fn_data)
 print(type(fn_data))
-# rdd = sc.parallelize(json_data)
+
+
+# def process_imdb_venice1(json_str):
+#     try:
+#         json_obj = json.loads(json_str)
+#         venice_awards_list = json_obj["nomineesWidgetModel"]["eventEditionSummary"]["awards"]
+#         tmp_nominee_list = []
+#         for i in venice_awards_list:
+#             if i["awardName"] == "Golden Lion":
+#                 tmp_nominee_list.append(i["categories"][0]["nominations"])
+#             else:
+#                 pass
+#
+#         nominee_list = tmp_nominee_list[0]
+#
+#         for i in nominee_list:
+#             if i["isWinner"] is True:
+#                 return i["primaryNominees"][0]["originalName"]
+#         return None
+#     except:
+#         return None
+#
+#
+# spark = SparkSession.builder.appName("IMDBVeniceProcessing").getOrCreate()
+#
+# udf_process_imdb_venice1 = udf(process_imdb_venice1, StringType())
+#
+#
+# json_data_list = []
+# years = ["1961","1962","1963","1964","1965","1966","1967","1968"]
+# for i in years:
+#     file_path = f"IMDb/imdb_venice_{i}.json"
+#     json_data_list.append(s3_file_to_json(file_path))
+#
+# schema = StructType([StructField("json_data", StringType(), True)])
+# json_df = spark.createDataFrame(json_data_list, schema)
+# result_df = json_df.withColumn("movie_nm", udf_process_imdb_venice1(json_df["json_data"]))
+# # 결과를 저장하거나 출력
+# result_df.show()
+#
+# # SparkSession 종료
+# spark.stop()
+
+
+
