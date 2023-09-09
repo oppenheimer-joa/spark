aa33d43563fae1351ba4ff2f7c1d9919ebff2aaf
blahxblah <id.blahxblah@gmail.com>
2023-09-09 13:58:22 +0900

    230909/blahxblah spark submit script 추가(경로설정 필요)

---
+++ sh/tmdb_pyspark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 실행 커맨드 = sh pyspark.sh $SPARK_FILE $SPARK_HOME
+# #SPARK_FILE은 파일의 절대 경로 입력
+
+# SPARK FILE 가져오기
+SPARK_HOME='/home/ubuntu/spark/spark-3.2.4'
+SPARK_FILE="$1"
+DATE="$2"
+MOVIE_CODE="$3"
+CATEGORY="$4"
+
+
+
+# Spark Job 제출
+# 각자의 spark 환경에 맞게 설정 필요
+# : 절대경로, master 링크, executor 메모리 및 코어
+$SPARK_HOME/bin/spark-submit --master spark://master:7077 $HOME/sms/test/src/$SPARK_FILE $DATE $MOVIE_CODE $CATEGORY
