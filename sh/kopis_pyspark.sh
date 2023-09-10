a9dce638ef7a80e852dd85ade1ed0fcedc1d29f2
blahxblah <id.blahxblah@gmail.com>
2023-09-10 11:10:48 +0900

    230911/blahxblah sh 파일 수정

--- sh/kopis_pyspark.sh
+++ sh/kopis_pyspark.sh
@@ -7,4 +7,4 @@ date="$1"
 # Spark Job 제출
 # 각자의 spark 환경에 맞게 설정 필요
 # : 절대경로, master 링크, executor 메모리 및 코어
-$SPARK_HOME/bin/spark-submit --master spark://master:7077 src/Kopis.py $date
+$SPARK_HOME/bin/spark-submit --master spark://master:7077 src/Kopis_transform_data.py $date
