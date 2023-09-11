f6716e8b3607fb35d18120cab4a397463a1d9193
J3llyBe4n <zza0423@gmail.com>
2023-09-11 15:09:31 +0900

    230911/yoda people 데이터 df 변환 완료

--- sh/tmdb_people_pyspark.sh
+++ sh/tmdb_people_pyspark.sh
@@ -2,10 +2,9 @@
 
 # SPARK FILE 가져오기
 SPARK_HOME='/home/ubuntu/spark/spark-3.2.4'
-SPARK_FILE="$1"
 DATE="$2"
 
 # Spark Job 제출
 # 각자의 spark 환경에 맞게 설정 필요
 # : 절대경로, master 링크, executor 메모리 및 코어
-$SPARK_HOME/bin/spark-submit --master spark://master:7077 $SPARK_FILE $DATE
+$SPARK_HOME/bin/spark-submit --master spark://master:7077 src/Tmdb_transform_people.py $DATE
--- src/Tmdb_transform_people.py
+++ src/Tmdb_transform_people.py
@@ -23,18 +23,22 @@ s3_files = spark.sparkContext.wholeTextFiles(f's3a://sms-basket/TMDB/people/{dat
 def transform_TMDB_people_json(json_data):
     try:
         data = json.loads(json_data)
+        people_id = data.get("id", "")
+        people_img = data.get("profile_path", "")
+        people_birth = data.get("birthday", "")
+        people_death = data.get("deathday", "")
         people_name = data.get("name", "")
         people_role = data.get("known_for_department", "")
-        return (people_name, people_role, date)
+        return (people_id, date_gte, people_name, people_role, people_img, people_birth, people_death)
     except json.JSONDecodeError as e:
         return (f"json decode err: {e}")
 
 transformed_people_rdd = s3_files.values().map(transform_TMDB_people_json)
 
-people_df = spark.createDataFrame(transformed_people_rdd, ["name", "known_for_department", "date_gte"])
+people_df = spark.createDataFrame(transformed_people_rdd, ["id", "date_gte", "name", "known_for_department", "profile_img", "birth", "death"])
 
 # S3에 parquet 데이터 저장
-s3_path = f's3a://sms-warehouse/TMDB/people'
+s3_path = f's3a://sms-warehouse/temp/people'
 filename = f'people_{date}'
 
 people_df.write.parquet(f'{s3_path}/{filename}')
