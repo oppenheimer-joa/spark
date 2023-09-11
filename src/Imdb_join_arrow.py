5443116a13db7072ebef3d06a55a6008dbddf6d2
woorek <lkjwj95@gmail.com>
2023-09-11 19:46:55 +0900

    230911/woorek 오탈자 수정

--- src/Imdb_join_arrow.py
+++ src/Imdb_join_arrow.py
@@ -20,7 +20,7 @@ spark = SparkSession.builder \
 
 # s3 경로 설정
 s3_path = "s3a://sms-warehouse/imdb/transformed-data"
-s3_whole_path_arrow = "s3a://sms-warehouse/imdb/all/arrow"
+s3_whole_path_arrow = "s3a://sms-warehouse/imdb/all/arrow/"
 
 # s3 path 하위에 있는 모든 폴더에 존재하는 모든 Parquet 파일을 하나의 데이터프레임으로 읽기
 df = spark.read.option("header", "true").parquet(s3_path + "/*/*/*/*.parquet")
