fa93111ee1ab34c591a7ea5bd85cf8d09bfd4165
J3llyBe4n <zza0423@gmail.com>
2023-09-12 12:49:56 +0900

    230912/yoda 파티셔닝 데이터 읽어오기

--- src/BoxOffice_transform_data.py
+++ src/BoxOffice_transform_data.py
@@ -23,10 +23,19 @@ date = '2023-07-31'
 
 # 박스오피스 wholeText로 다 가져오고, 거기서 앞에 날짜를 기반으로 해당 json만 읽어오기
 s3_path = spark.sparkContext.wholeTextFiles(f"s3a://sms-basket/kobis/{date.split('-')[0]}")
+filtered_files = s3_path.filter(lambda filter_data: date.replace('-', '') in filter_data[0])
 
-filtered_files = s3_path.filter(lambda filter_data: date.replace('-', '') in filter_dat[0])
-filtered_datas = filtered_files.collect().keys()
-print(filterd_datas)
 # 읽은 json 파일 dataframe으로 전부다 합치기 지역코드 추가해야함
 
+def transform_boxOffice_data(json_data):
+    try:
+        data = json.loads(json_data)
+        return data
+    except json.JSONDecodeError as e:
+        return (f"json decode err : {e}")
+
+
 # 만들어진 df를 temp에 떨어뜨려야함
+
+tmp = filtered_files.values().map(transform_boxOffice_data)
+tmp.foreach(print)
