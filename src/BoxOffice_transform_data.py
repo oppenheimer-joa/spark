de9e1df99bdab298df5cb0e2db57328bd3b47747
J3llyBe4n <zza0423@gmail.com>
2023-09-12 13:44:38 +0900

    test23

--- src/BoxOffice_transform_data.py
+++ src/BoxOffice_transform_data.py
@@ -21,15 +21,17 @@ spark = SparkSession.builder \
 date = '2023-07-31'
 
 # 박스오피스 wholeText로 다 가져오고, 거기서 앞에 날짜를 기반으로 해당 json만 읽어오기
-s3_path = spark.sparkContext.wholeTextFiles(f"s3a://sms-basket/kobis/{date.split('-')[0]}")
-filtered_files = s3_path.filter(lambda filter_data: date.replace('-', '') in filter_data[0])
+#s3_path = spark.sparkContext.wholeTextFiles(f"s3a://sms-basket/kobis/{date.split('-')[0]}")
+#filtered_files = s3_path.filter(lambda filter_data: date.replace('-', '') in filter_data[0])
+s3_path = spark.sparkContext.wholeTextFiles(f"s3a://sms-basket/kobis/20230731_0105001_boxOffice.json")
+
 
 # 읽은 json 파일 dataframe으로 전부다 합치기 지역코드 추가해야함
 
 def transform_boxOffice_data(json_data):
     try:
         data = json.loads(json_data)
-        print(type(data))
+        print(json_data)
         print(data)
         result = data.get("boxOfficeResult", {}).get("dailyBoxOfficeList", [])
 
@@ -40,4 +42,10 @@ def transform_boxOffice_data(json_data):
 
 # 만들어진 df를 temp에 떨어뜨려야함
 
-tmp = filtered_files.values().map(transform_boxOffice_data)
+#tmp = filtered_files.values().map(transform_boxOffice_data)
+tmp = s3_path.values().map(transform_boxOffice_data)
+
+
+
+
+
