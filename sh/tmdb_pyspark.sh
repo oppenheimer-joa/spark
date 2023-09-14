80008b94d0786fc12c6d09345b7f5814af1251c9
J3llyBe4n <zza0423@gmail.com>
2023-09-14 10:51:40 +0900

    230914/yoda sparklocal 서버 경로 수정

--- sh/tmdb_pyspark.sh
+++ sh/tmdb_pyspark.sh
@@ -4,7 +4,7 @@
 # #SPARK_FILE은 파일의 절대 경로 입력
 
 # SPARK FILE 가져오기
-SPARK_HOME='/home/ubuntu/spark/spark-3.2.4'
+SPARK_HOME='/home/spark/spark/spark-3.2.4'
 SPARK_FILE="$1"
 DATE="$2"
 MOVIE_CODE="$3"
@@ -12,4 +12,4 @@ MOVIE_CODE="$3"
 # Spark Job 제출
 # 각자의 spark 환경에 맞게 설정 필요
 # : 절대경로, master 링크, executor 메모리 및 코어
-$SPARK_HOME/bin/spark-submit --master spark://master:7077 $SPARK_FILE $DATE $MOVIE_CODE
+$SPARK_HOME/bin/spark-submit --master spark://master:7077 --total-executor-cores=2 $SPARK_FILE $DATE $MOVIE_CODE
