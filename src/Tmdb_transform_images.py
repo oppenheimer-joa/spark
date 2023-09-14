f600f436ad63844225224d4266c18620752681b4
J3llyBe4n <zza0423@gmail.com>
2023-09-14 11:01:31 +0900

    230914/yoda TMDB_image 고도화

--- src/Tmdb_transform_images.py
+++ src/Tmdb_transform_images.py
@@ -19,32 +19,34 @@ spark = SparkSession.builder \
 # movie_code = '1000336'
 # Airflow 에서 받을 파라미터
 date = sys.argv[1]
-movie_code = sys.argv[2]
 category = 'image'
 
-image_path = make_tmdb_file_dir(category, date, movie_code)
-image_data = get_TMDB_data(image_path)
-
-raw_image_rdd = spark.sparkContext.parallelize([image_data])
-raw_image_rdd.foreach(print)
+s3_files = spark.sparkContext.wholeTextFiles(f's3a://sms-basket/TMDB/{category}/{date}')
 
 #image 전처리 함수
 def transform_TMDB_image_json(json_data):
     try:
         data = json.loads(json_data)
         posters = data.get("posters", [])
+        movie_id = data.get("id", "")
         if posters:
             poster_file_path = posters[0].get("file_path", "")
-            data["posters"] = poster_file_path
+        else:
+            poster_file_path = ""
             
-        return json.dumps(data)
+        return (movie_id, poster_file_path)
 
     except json.JSONDecodeError as e:
         return f"JSON decoding error: {e}"
 
-transformed_image_rdd = raw_image_rdd.map(transform_TMDB_image_json)
+transformed_image_rdd = s3_files.values().map(transform_TMDB_image_json)
+
+image_df = spark.createDataFrame(transformed_image_rdd, ["id", "posters"])
 
 # S3에 rdd 데이터 transformed__rdd 저장
 s3_path = f's3a://sms-warehouse/temp'
-filename = f'image_{date}_{movie_code}'
-transformed_image_rdd.saveAsTextFile(f"{s3_path}/{filename}")
+filename = f'image_{date}'
+image_df.write.mode("overwrite").parquet(f'{s3_path}/{filename}')
+
+
+
